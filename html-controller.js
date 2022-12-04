227299f740bd03e18274b0c32748f22449fa36c2
skparab1 <skparab1@gmail.com>
2022-12-03 17:59:11 -0800

    ok

--- html-controller.js
+++ html-controller.js
@@ -180,6 +180,8 @@ function settheme(timediff){
       r.style.setProperty('--slight', holder);
       holder = rs.getPropertyValue('--darkunderline')
       r.style.setProperty('--underline', holder);
+      holder = rs.getPropertyValue('--lightmain')
+      r.style.setProperty('--opposite', holder);
     } else {
       let holder = rs.getPropertyValue('--lightbg')
       r.style.setProperty('--bg', holder);
@@ -193,6 +195,8 @@ function settheme(timediff){
       r.style.setProperty('--slight', holder);
       holder = rs.getPropertyValue('--lightunderline')
       r.style.setProperty('--underline', holder);
+      holder = rs.getPropertyValue('--darkaccent')
+      r.style.setProperty('--opposite', holder);
     }
 
   }
